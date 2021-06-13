bc52e8db14dfc30c7acde913167e3fd8fa17f6e8
Atul Kumar <mycodingx@gmail.com>
2021-06-13 17:47:03 +0530

    adding first styled component as app container

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import styled from "styled-components";
 import "./App.css";
 
+const AppContainer = styled.div`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  justify-content: center;
+  margin-top: 7em;
+`;
+
 function App() {
-  return <div>Hello World..!!</div>;
+  return <AppContainer>Hello World..!!</AppContainer>;
 }
 
 export default App;
